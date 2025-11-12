4ab0b5a2aa3cd7f4bcdbbac19d5cb0fe466e6626
DumitruA19 <95125201+DumitruA19@users.noreply.github.com>
2025-11-12 14:46:41 +0200

    Comment out dataframe display and stop functions
    Comment out dataframe display and stop calls while keeping the conversion to Pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ session = st.connection("snowflake").session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use th LOC function
-# pd_df=my_dataframe.to_pandas()
+ pd_df=my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
@@ -41,7 +41,7 @@ if ingredients_list:
         ingredients_string += fruits_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
 
         st.subheader(fruit_chosen + 'Nutrition Information')
